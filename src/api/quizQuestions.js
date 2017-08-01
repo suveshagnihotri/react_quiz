2e5688638fa695a794b658b8b2a828edc48c5ab5
Suvesh Agnihotri <suveshagnihotri145@gmail.com>
2017-08-02 01:49:55 +0530

    inital commit

---
+++ src/api/quizQuestions.js
@@ -0,0 +1,93 @@
+var quizQuestions = [
+  {
+      question: "The nurse has given the male client with Bell’s palsy instructions on preserving muscle tone in the face and preventing denervation. The nurse determines that the client needs additional information if the client states that he or she will: ",
+      answers: [
+          {
+              type: "Microsoft",
+              content: "Exposure to cold and drafts"
+          },
+          {
+              type: "Nintendo",
+              content: "Massage the face with a gentle upward motion"
+          },
+          {
+              type: "Sony",
+              content: "Perform facial exercises"
+          },
+            {
+              type: "Suveshs",
+              content: "Wrinkle the forehead, blow out the cheeks, and whistle"
+          }
+      ]
+  },
+  {
+      question: "Which console would you prefer to play with friends?",
+      answers: [
+          {
+              type: "Microsoft",
+              content: "X-Box"
+          },
+          {
+              type: "Nintendo",
+              content: "Nintendo 64"
+          },
+          {
+              type: "Sony",
+              content: "Playstation 1"
+          }
+      ]
+  },
+  {
+      question: "Which of these racing franchises would you prefer to play a game from?",
+      answers: [
+          {
+              type: "Microsoft",
+              content: "Forza"
+          },
+          {
+              type: "Nintendo",
+              content: "Mario Kart"
+          },
+          {
+              type: "Sony",
+              content: "Gran Turismo"
+          }
+      ]
+  },
+  {
+      question: "Which of these games do you think is best?",
+      answers: [
+          {
+              type: "Microsoft",
+              content: "BioShock"
+          },
+          {
+              type: "Nintendo",
+              content: "The Legend of Zelda: Ocarina of Time"
+          },
+          {
+              type: "Sony",
+              content: "Final Fantasy VII"
+          }
+      ]
+  },
+  {
+      question: "What console would you prefer to own?",
+      answers: [
+          {
+              type: "Microsoft",
+              content: "X-Box One"
+          },
+          {
+              type: "Nintendo",
+              content: "Wii U"
+          },
+          {
+              type: "Sony",
+              content: "Playstation 4"
+          }
+      ]
+  }
+];
+
+export default quizQuestions;
